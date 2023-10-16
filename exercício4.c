920f58c83ea5e970a0222c2cfbfc86e979242dcf
HenriqueMedeirosCosta <132111448+HenriqueMedeirosCosta@users.noreply.github.com>
2023-10-16 09:52:12 -0300

    Create exercício4.c

---
+++ exercício4.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+// Faça um algoritmo que leia um número positivo e imprima seus divisores. Exemplo: Os divisores do número 66 são: 1, 2, 3, 6, 11, 22, 33 e 66.
+
+int main() {
+    int numero;
+    
+    printf("Digite um numero positivo: ");
+    scanf("%d", &numero);
+
+    if (numero <= 0) {
+        printf("O numero inserido nao e positivo.\n");
+    } else {
+        printf("Os divisores do numero %d sao: ", numero);
+
+        for (int i = 1; i <= numero; i++) {
+            if (numero % i == 0) {
+                printf("%d", i);
+                if (i != numero) {
+                    printf(", ");
+                }
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
