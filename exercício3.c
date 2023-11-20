501ef19d26e099ad259c4862f7c311ca1630189f
HenriqueMedeirosCosta <132111448+HenriqueMedeirosCosta@users.noreply.github.com>
2023-11-19 22:18:34 -0300

    exercício3.c

--- exercício3.c
+++ exercício3.c
@@ -1,22 +1,32 @@
-#include<stdio.h>
-#include <stdlib.h>
+#include <stdio.h>
 
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    } else {
+        int a = 0, b = 1, temp;
+        for (int i = 2; i <= n; i++) {
+            temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
+}
 
-main() {
-	int n; 
-	int seq = 1, seqA = 0, seqAux; 
-	printf("Quantos termos da sequencia de Fibonacci deseja ver? ");
-	scanf("%d", &n);
-	
-	printf("\n0 ");
-	n--;
-	
-	while (n) {
-		printf("%d ", seq);
-		seqAux = seq;
-		seq += seqA;
-		seqA = seqAux;
-		n--;
-	}	  
-	printf("\n\n");
+int main() {
+    int n;
+
+    printf("Digite um número inteiro não negativo: ");
+    scanf("%d", &n);
+
+    if (n < 0) {
+        printf("O número deve ser não negativo.\n");
+        return 1; // Indica um erro
+    }
+
+    int resultado = fibonacci(n);
+    printf("O %d-ésimo termo da sequência de Fibonacci é: %d\n", n, resultado);
+
+    return 0; // Indica sucesso
 }
